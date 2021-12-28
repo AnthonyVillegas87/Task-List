0bfe74c2ccedd2c328eba4cfb87224d52a4e6421
Anthony Villegas <villegasanthony87@yahoo.com>
2021-12-28 12:30:34 -0600

    feat: functionality to add, remove, clear all, & filter tasks on list

--- js/app.js
+++ js/app.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
     //Add Task event
     form.addEventListener('submit', addTask);
+    //Remove task event
+    taskList.addEventListener('click', removeTask);
+    //Clear task event
+    clearBtn.addEventListener('click', clearTasks);
+    //Filter tasks event
+    filter.addEventListener('keyup', filterTasks)
 }
 
 //Add Task function
@@ -30,7 +36,7 @@ function addTask(e) {
     //Add class
     link.className = 'delete-item secondary-content';
     //Add icon HTML
-    link.innerHTML = '<i class="fas fa-minus"></i>';
+    link.innerHTML = '<i class="fas fa-trash-alt"></i>';
     //Append the link to li
     li.appendChild(link);
 
@@ -40,4 +46,33 @@ function addTask(e) {
     //Clear input
     taskInput.value = '';
     e.preventDefault();
+}
+
+//Remove Task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure you want to Remove this Item?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+//Clear Tasks
+function clearTasks() {
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+//Filter tasks
+function filterTasks(e){
+    const text = e.target.value.toLowerCase();
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+        if(item.toLowerCase().indexOf(text) !== -1) {
+           task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
